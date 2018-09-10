0ed004f71aa383d29692f417e8dace4c570d5ff9
Philip Fulgham <philfulgham@gmail.com>
2018-09-09 21:29:18 -0400

    Add solved state animation to reset button

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Input } from "@angular/core";
+import { Component, ElementRef, Input, OnChanges } from "@angular/core";
 import { TweenMax } from "gsap";
 
 @Component({
@@ -6,8 +6,13 @@ import { TweenMax } from "gsap";
 	templateUrl: "./button.component.html",
 	styleUrls: ["./button.component.scss"],
 })
-export class ButtonComponent {
-	@Input() text: string;
+export class ButtonComponent implements OnChanges {
+	@Input()
+	public highlight: boolean;
+	@Input()
+	public text: string;
+
+	private flashAnimation: TweenMax;
 
 	private get elem(): HTMLElement {
 		return this._elem.nativeElement;
@@ -15,7 +20,32 @@ export class ButtonComponent {
 
 	constructor(private _elem: ElementRef) {}
 
+	ngOnChanges() {
+		if (this.highlight) {
+			this.startFlashing();
+		} else {
+			this.stopFlashing();
+		}
+	}
+
 	public onClick() {
-		TweenMax.to(this.elem, 0.25, { scale: 0.95 }).repeat(1).yoyo(true);
+		TweenMax.to(this.elem, 0.25, { scale: 0.95 })
+			.repeat(1)
+			.yoyo(true);
+	}
+
+	private startFlashing() {
+		return (this.flashAnimation = TweenMax.to(this.elem.querySelector("div"), 0.25, { className: "+=flash" })
+			.repeat(-1)
+			.yoyo(true));
+	}
+
+	private stopFlashing() {
+		if (this.flashAnimation) {
+			this.flashAnimation.kill();
+		}
+
+		this.flashAnimation = undefined;
+		return TweenMax.to(this.elem.querySelector("div"), 0.1, { className: "-=flash" });
 	}
 }
