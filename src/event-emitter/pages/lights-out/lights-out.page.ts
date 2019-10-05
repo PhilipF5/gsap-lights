ede0f4cec1c4c6198064c66e30d90a8fc843fd56
peterennis <peterennis@yahoo.com>
2019-10-04 21:37:02 -0700

    fix(): ng update @angular/core fixes Static Query Migration

--- src/event-emitter/pages/lights-out/lights-out.page.ts
+++ src/event-emitter/pages/lights-out/lights-out.page.ts
@@ -7,7 +7,7 @@ import { LightsGridComponent } from "event-emitter/components";
 	styleUrls: ["./lights-out.page.scss"],
 })
 export class LightsOutPage {
-	@ViewChild(LightsGridComponent)
+	@ViewChild(LightsGridComponent, { static: true })
 	public grid: LightsGridComponent;
 
 	public get gridSolved() {
